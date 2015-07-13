5156706e3eb8768032aa5e0079b0750deb30d5be
root <root@localhost.localdomain>
2015-07-13 14:51:02 +0800

    功能基本完善

--- epoll1.py
+++ epoll1.py
@@ -8,14 +8,16 @@ header  = b'HTTP/1.0 200 OK\r\n'
 header += b'Content-Type: text/html\r\nConnection:keep-alive\r\ncharset=UTF-8\r\n\r\n'
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serversocket.bind(('0.0.0.0', 8888))
+serversocket.bind(('0.0.0.0', 8899))
 serversocket.listen(1)
 serversocket.setblocking(0)
 epoll = select.epoll()
 epoll.register(serversocket.fileno(), select.EPOLLIN)
 cwd=os.getcwd()
 
-def dealphp(path,url,method,request):
+
+##外部调用php或python文件等各种脚本文件
+def dealphp(path,url,method,request,types):
     if method=='GET':
         if len(url.split('?'))>1:
             can=url.split('?')[1]
@@ -26,30 +28,66 @@ def dealphp(path,url,method,request):
             while i<len(a):
                 b=b+' '+a[i]
                 i=i+1
-            v=os.popen('php '+cwd+path+b)
-            return v.read()
+            if types=='py':
+                v=os.popen('python '+cwd+path+b)
+            elif types=='c':
+                os.system('gcc '+cwd+path+' -o '+cwd+path[0:-2])
+                v=os.popen(cwd+path[0:-2]+b).read()
+                os.system('rm '+cwd+path[0:-2]+' -f')
+            else:
+                v=os.popen(types+' '+cwd+path+b)
+            return header+v.read()
         else:
-            return os.popen('php '+cwd+path).read()
+            if types=='py':
+                v=os.popen('python '+cwd+path)
+            elif types=='c':
+                os.system('gcc '+cwd+path+' -o '+cwd+path[0:-2])
+                v=os.popen(cwd+path[0:-2]).read()
+                os.system('rm '+cwd+path[0:-2]+' -f') 
+            else:
+                v=os.popen(types+' '+cwd+path)
+            return header+v.read()
     else:
         po=request.split('\n\r\n')[1]
-        print po 
-        return os.popen('php '+cwd+path).read()       
-def dealpy(path):
-    pass
+        if po!='':
+            print "po"
+            cann=po.split('&')
+            print 'cann'
+            a=[i.split('=')[1] for i in cann]
+            print a
+            b=' '
+            i=0
+            while i<len(a):
+                b=b+' '+a[i]
+                i=i+1
+            print 'B'
+            print b
+            if types=='py':
+                v=os.popen('python '+cwd+path+b).read()
+            elif types=='c':
+                os.system('gcc '+cwd+path+' -o '+cwd+path[0:-2])
+                v=os.popen(cwd+path[0:-2]+b).read()
+                os.system('rm '+cwd+path[0:-2]+' -f')
+            else:
+                v=os.popen(types+' '+cwd+path+b).read()
+            v=urllib.unquote(v)
+            return header+v       
 
+##外部调用html文件
 def dealhtml(path):
     f=open(cwd+path,"rb")
     y=f.read()
     y=header+y
     return y
 
+##外部调用js和css文件
 def dealjs(path):
     f=open(cwd+path,"rb")
     y=f.read()
     f.close()
     return y
 
-
+##处理的到的请求
 def dealresponse(request):
     method=request.split(' ')[0]
     url=request.split(' ')[1]
@@ -65,19 +103,20 @@ def dealresponse(request):
                 types=path.split('.')[-1]
                 if types=='html':
                     return dealhtml(path)
-                elif types=='php':
-                    return dealphp(path,url,method,request)
+                elif types=='php' or types=='py' or types=='c' or types=='sh':
+                    return dealphp(path,url,method,request,types)
                 elif (types=='js' or types=='css'):
                     return dealjs(path)
-                elif types=='py':
-                    return dealpy(path)
                 else:
                     f=open(cwd+path,"rb")
                     x=f.read()
                     f.close()
                     return x
             else:
-                return "  "   
+                f=open("index.html","rb")
+                x=header+f.read()
+                f.close()
+                return x  
 
 
 
@@ -88,18 +127,17 @@ try:
     connections = {}; requests = {}; responses = {};connstream={}
     while True:
         events = epoll.poll(1)
-        print events
         for fileno, event in events:
             print events
             if fileno == serversocket.fileno():
                 print ("a")
                 connection, address = serversocket.accept()
                 connection.setblocking(0)
-                print connection.fileno()
-                
-                #connection.setblocking(0)
+                print connection.fileno()               
+                connection.setblocking(0)
                 epoll.register(connection.fileno(), select.EPOLLIN)
                 connstream[connection.fileno()] = context.wrap_socket(connection, server_side=True,do_handshake_on_connect=False)
+               # connstream[connection.fileno()].setblocking(0)
                 connections[connection.fileno()] = connection
                 requests[connection.fileno()] = b''
                 responses[connection.fileno()] = b''
@@ -107,9 +145,8 @@ try:
                 print("b")
                 while True:
                     try:
-                        #connstream[fileno].do_handshake()
+                        connstream[fileno].do_handshake()
                         requests[fileno] += connstream[fileno].recv(1024)
-                        print "ffffff"+requests[fileno]
                         break
                     except ssl.SSLWantReadError:
                         #print("sslerror")
@@ -117,34 +154,29 @@ try:
                     except:
                         print("f")
                         epoll.modify(fileno,0)
-                        #pass
                         break
                 print('yes')
                 if requests[fileno]=='':
                     epoll.unregister(fileno)
                 if EOL1 in requests[fileno] or EOL2 in requests[fileno]:
-                #if True:
                     epoll.modify(fileno, select.EPOLLOUT)
-                    requests[fileno]=urllib.unquote(requests[fileno].decode())
                     print('-'*40 + '\n' + requests[fileno])
-             
                     responses[fileno]=dealresponse(requests[fileno])
-                    #print(responses[fileno])
-                    #responses[fileno]=header
             elif event & select.EPOLLOUT:
                 print ("c")
                 while True:
                     try:
                         print ("try")
-                        #connstream[fileno].do_handshake()
+                        connstream[fileno].do_handshake()
                         byteswritten = connstream[fileno].send(responses[fileno])
                         responses[fileno] = responses[fileno][byteswritten:]
                         break
                     except ssl.SSLWantReadError:
                         print ("wanterror")
                         pass
-                   
-                responses[fileno] = responses[fileno][byteswritten:]
+                    except :
+                        epoll.modify(fileno,0)                        
+                        break
                 if len(responses[fileno]) == 0:
                     epoll.modify(fileno, 0)
                     try:
@@ -157,12 +189,9 @@ try:
                         pass
             elif event & select.EPOLLHUP:
                 print ("d")
-                epoll.unregister(fileno)
-               
-                connstream[fileno].close()
-               
+                epoll.unregister(fileno)               
+                connstream[fileno].close()              
                 connections[fileno].close()
-                
                 del connections[fileno]
                 del connstream[fileno]
 finally:
