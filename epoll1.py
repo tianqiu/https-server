24658bee6ae22b73c35a54338a256cc443802f1b
root <root@localhost.localdomain>
2015-07-13 09:17:03 +0800

    修改了epll模型，增加了上传音乐

--- epoll1.py
+++ epoll1.py
@@ -15,21 +15,25 @@ epoll = select.epoll()
 epoll.register(serversocket.fileno(), select.EPOLLIN)
 cwd=os.getcwd()
 
-def dealphp(path,url):
-    if len(url.split('?'))>1:
-        can=url.split('?')[1]
-        cann=can.split('&')
-        a=[i.split('=')[1] for i in cann]
-        b=' '
-        i=0
-        while i<len(a):
-            b=b+' '+a[i]
-            i=i+1
-        v=os.popen('php '+cwd+path+b)
-        return v.read()
+def dealphp(path,url,method,request):
+    if method=='GET':
+        if len(url.split('?'))>1:
+            can=url.split('?')[1]
+            cann=can.split('&')
+            a=[i.split('=')[1] for i in cann]
+            b=' '
+            i=0
+            while i<len(a):
+                b=b+' '+a[i]
+                i=i+1
+            v=os.popen('php '+cwd+path+b)
+            return v.read()
+        else:
+            return os.popen('php '+cwd+path).read()
     else:
-        return os.popen('php '+cwd+path).read()
-
+        po=request.split('\n\r\n')[1]
+        print po 
+        return os.popen('php '+cwd+path).read()       
 def dealpy(path):
     pass
 
@@ -62,29 +66,40 @@ def dealresponse(request):
                 if types=='html':
                     return dealhtml(path)
                 elif types=='php':
-                    return dealphp(path,url)
+                    return dealphp(path,url,method,request)
                 elif (types=='js' or types=='css'):
                     return dealjs(path)
                 elif types=='py':
                     return dealpy(path)
+                else:
+                    f=open(cwd+path,"rb")
+                    x=f.read()
+                    f.close()
+                    return x
             else:
-                return " "   
+                return "  "   
 
 
 
 
 ################################################################################
 try:
-    connections = {}; requests = {}; responses = {}
+    print serversocket.fileno()
+    connections = {}; requests = {}; responses = {};connstream={}
     while True:
         events = epoll.poll(1)
+        print events
         for fileno, event in events:
+            print events
             if fileno == serversocket.fileno():
                 print ("a")
                 connection, address = serversocket.accept()
-                connstream = context.wrap_socket(connection, server_side=True,do_handshake_on_connect=False)
                 connection.setblocking(0)
+                print connection.fileno()
+                
+                #connection.setblocking(0)
                 epoll.register(connection.fileno(), select.EPOLLIN)
+                connstream[connection.fileno()] = context.wrap_socket(connection, server_side=True,do_handshake_on_connect=False)
                 connections[connection.fileno()] = connection
                 requests[connection.fileno()] = b''
                 responses[connection.fileno()] = b''
@@ -92,19 +107,27 @@ try:
                 print("b")
                 while True:
                     try:
-                        connstream.do_handshake()
-                        requests[fileno] += connstream.recv(1024)
+                        #connstream[fileno].do_handshake()
+                        requests[fileno] += connstream[fileno].recv(1024)
+                        print "ffffff"+requests[fileno]
                         break
                     except ssl.SSLWantReadError:
+                        #print("sslerror")
                         pass
                     except:
-                        epoll.modify(fileno, 0)
+                        print("f")
+                        epoll.modify(fileno,0)
+                        #pass
                         break
                 print('yes')
+                if requests[fileno]=='':
+                    epoll.unregister(fileno)
                 if EOL1 in requests[fileno] or EOL2 in requests[fileno]:
+                #if True:
                     epoll.modify(fileno, select.EPOLLOUT)
                     requests[fileno]=urllib.unquote(requests[fileno].decode())
                     print('-'*40 + '\n' + requests[fileno])
+             
                     responses[fileno]=dealresponse(requests[fileno])
                     #print(responses[fileno])
                     #responses[fileno]=header
@@ -112,16 +135,22 @@ try:
                 print ("c")
                 while True:
                     try:
-                        connstream.do_handshake()
-                        byteswritten = connstream.send(responses[fileno])
-                           # responses[fileno] = responses[fileno][byteswritten:]
+                        print ("try")
+                        #connstream[fileno].do_handshake()
+                        byteswritten = connstream[fileno].send(responses[fileno])
+                        responses[fileno] = responses[fileno][byteswritten:]
                         break
-                    except ssl.SSLWantWriteError:
+                    except ssl.SSLWantReadError:
+                        print ("wanterror")
                         pass
+                   
                 responses[fileno] = responses[fileno][byteswritten:]
                 if len(responses[fileno]) == 0:
                     epoll.modify(fileno, 0)
-                    connstream.shutdown(socket.SHUT_RDWR)
+                    try:
+                        connstream[fileno].shutdown(socket.SHUT_RDWR)
+                    except:
+                        pass
                     try:
                         connections[fileno].shutdown(socket.SHUT_RDWR)
                     except:
@@ -129,12 +158,13 @@ try:
             elif event & select.EPOLLHUP:
                 print ("d")
                 epoll.unregister(fileno)
-                connstream.close()
-                try:
-                    connections[fileno].close()
-                except: 
-                    pass
+               
+                connstream[fileno].close()
+               
+                connections[fileno].close()
+                
                 del connections[fileno]
+                del connstream[fileno]
 finally:
     print ("e")
     epoll.unregister(serversocket.fileno())
